d9e5f7fa090463f7f1699230447073aeda856b49
Glendon Philipp Baculio <gpbaculio@gmail.com>
2019-04-06 16:26:27 +0800

    markdown previewer setup ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { FaFreeCodeCamp } from 'react-icons/fa';
+import marked from 'marked';
 import './App.css';
+import { markdownInitialText } from './constants';
+interface AppProps {}
+interface AppState {
+  [markdown: string]: string;
+}
+class App extends Component<AppProps, AppState> {
+  constructor(props: AppProps) {
+    super(props);
 
-class App extends Component {
+    this.state = {
+      markdown: markdownInitialText
+    };
+  }
+
+  markdownChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const { value, name } = e.target;
+    this.setState({
+      [name]: value
+    });
+  };
   render() {
+    const { markdown } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='App container'>
+        <div className='input-group w-75 m-auto h-25'>
+          <div className='input-group-prepend'>
+            <div className='input-group-text'>
+              <FaFreeCodeCamp size={36} />
+              <span className='ml-2 editor'>Editor</span>
+            </div>
+          </div>
+          <textarea
+            name='markdown'
+            value={markdown}
+            className='form-control'
+            aria-label='With textarea'
+            onChange={this.markdownChange}
+          />
+        </div>
+        <div className='mockdown-output'>Markdown Output</div>
+
+        <div dangerouslySetInnerHTML={{ __html: marked(markdown) }} />
       </div>
     );
   }
